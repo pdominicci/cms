75cf695115bfe8271a209b2efb0f1532ab0e7da5
Pali <pablodominicci@gmail.com>
2020-11-12 16:51:21 -0300

    ckeditor

--- public/static/js/admin.js
+++ public/static/js/admin.js
@@ -1,8 +1,12 @@
 var base = location.protocol+'//'+location.host;
 
+$(document).ready(function(){
+    editor_init()
+})
+
 function editor_init(field){
     CKEDITOR.replace(field,{
-        skin: '',
+        skin: 'moono',
         extraPlugins: 'codesnippet,ajax,xml,textmatch,autocomplete,textwatcher,emoji,panelbutton,preview,wordcount',
         toolbar: [
             { name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', '-', 'Undo', 'Redo']},
